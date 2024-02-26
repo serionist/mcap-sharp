78061ce11609ad8dc7b35f9be092b32228443126
Soma Osvay <osvays@starschema.net>
2024-02-26 03:01:22 +0100

    backendtime

--- MCAP-csharp/DataTypes/McapDateTime.cs
+++ MCAP-csharp/DataTypes/McapDateTime.cs
@@ -20,7 +20,7 @@ namespace MCAP_csharp.DataTypes
         public DateTime? ApproximateDateTime
         {
             get => NanoSeconds == 0 ? (DateTime?)null: new DateTime(1970, 1, 1).AddTicks((long)NanoSeconds / 100);
-            set => NanoSeconds = !value.HasValue ? 0: (ulong)value.Value.Ticks * 100;
+            set => NanoSeconds = !value.HasValue ? 0: (ulong)value.Value.Subtract(new DateTime(1970, 1, 1)).Ticks * 100;
         }
     }
 }
